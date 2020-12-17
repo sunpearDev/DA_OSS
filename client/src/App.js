bc6732633f48c4b9e50c3c0db78375c51837ba3f
tranthingocnhat <Dh51705101@student.stu.edu.vn>
2020-12-17 11:40:20 +0700

    merge order detail page voi master

--- client/src/App.js
+++ client/src/App.js
@@ -6,6 +6,12 @@ import Navbar from "./components/Navbar";
 import HomePage from "./pages/HomePage";
 
 
+<<<<<<< HEAD
+=======
+import GameDetailPage from "./pages/GameDetailPage";
+
+import OrderPage from "./pages/OrderPage";
+>>>>>>> oder_gamedetail_page
 import LibraryPage from "./pages/LibraryPage";
 import Footer from "./components/Footer";
 
@@ -17,7 +23,13 @@ function App() {
       <div className="container">
           <Route path="/" exact component={HomePage} />
           <Route path="/library" component={LibraryPage} />
+<<<<<<< HEAD
          
+=======
+          <Route path = "/order/:id" component = { OrderPage } />
+          <Route path = "/game/:id"
+        component = { GameDetailPage } />
+>>>>>>> oder_gamedetail_page
       </div>
       <Footer />
     </BrowserRouter>
