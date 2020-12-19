0958b657aaec6fcfab0199bac4384a6d5d3466f1
phamh <phamhung2015cb@gmail.com>
2020-12-19 14:48:28 +0700

    Sua loi browser

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import "./App.css";
 import { BrowserRouter, Route } from "react-router-dom";
 
+import BrowserPage from "./pages/BrowserPage"
 import Navbar from "./components/Navbar";
 import HomePage from "./pages/HomePage";
 
@@ -17,6 +18,7 @@ function App() {
       <div className="container">
           <Route path="/" exact component={HomePage} />
           <Route path="/library" component={LibraryPage} />
+		  <Route path="/browser" component={BrowserPage} />
          
       </div>
       <Footer />
