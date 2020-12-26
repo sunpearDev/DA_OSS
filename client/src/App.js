6860e4a38a5e6df2215154c2be21874ce113d965
tranthingocnhat <Dh51705101@student.stu.edu.vn>
2020-12-26 17:04:47 +0700

    hoanthanh nhanh chitiet

--- client/src/App.js
+++ client/src/App.js
@@ -6,14 +6,9 @@ import Navbar from "./components/Navbar";
 import HomePage from "./pages/HomePage";
 
 
-<<<<<<< HEAD
-=======
 import GameDetailPage from "./pages/GameDetailPage";
 
-import OrderPage from "./pages/OrderPage";
->>>>>>> oder_gamedetail_page
-import LibraryPage from "./pages/LibraryPage";
-import Footer from "./components/Footer";
+
 
 
 function App() {
@@ -22,16 +17,11 @@ function App() {
      <Navbar />
       <div className="container">
           <Route path="/" exact component={HomePage} />
-          <Route path="/library" component={LibraryPage} />
-<<<<<<< HEAD
-         
-=======
-          <Route path = "/order/:id" component = { OrderPage } />
+          
           <Route path = "/game/:id"
         component = { GameDetailPage } />
->>>>>>> oder_gamedetail_page
       </div>
-      <Footer />
+     
     </BrowserRouter>
   );
 }
---
+++ client/src/pages/HomePage.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import GameRow from '../components/GameRow'
+
+
+export default class HomePage extends Component {
+    render() {
+        return (
+            <>
+            
+                <GameRow title="All" />
+            </>
+        )
+    }
+}
